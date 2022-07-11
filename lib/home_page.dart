f2e35054e99e209c998de7b3e3b6efdedf0ec866
Tanisha Arora <tanishaarora137@gmail.com>
2022-07-11 11:31:36 +0530

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Tanisha";
+
+  return Scaffold(
+    appBar: AppBar(                                                  //head
+      title: Text("Catalog App"),
+    ),
+    body: Center(                                                    //body
+      child: Container(
+        child: Text("Welcome to 30 $days days of flutter by $name"),
+      ),
+    ),
+    drawer: Drawer(),
+  );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,17 @@
+import 'package:all_covered_in_one/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+//  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
-
-
-
